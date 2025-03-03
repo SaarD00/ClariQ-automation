5563f0d26294e527b07048ed8b0df6f9796a7ece
SaarD00ishuman <97211377+SaarD00ishuman@users.noreply.github.com>
2025-03-03 18:47:52 +0530

    yes

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
       {
-        protocol: 'https',
-        hostname: 'ucarecdn.com',
+        protocol: "https",
+        hostname: "ucarecdn.com",
       },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
